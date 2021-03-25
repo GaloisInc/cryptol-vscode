a8dafe0996e71cd35290a468d339c705958758cf
Ajay Eeralla <ajay.eeralla@gmail.com>
2021-03-25 16:17:01 -0700

    add some snippets

--- src/extension.ts
+++ src/extension.ts
@@ -9,6 +9,48 @@ interface CryptolState{
     cryptolTerminalOpen : boolean;
     loadedFile : string;
 }
+// show symbol definition 
+/*class GoDefinitionProvider implements vscode.DefinitionProvider{
+    public provideDefinition(
+        document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken):
+        Thenable<vscode.Location>{
+
+        }
+    )
+}*/
+
+// helper functions
+function selectionOrWord(currentEditor : vscode.TextEditor) : string {
+    let selectedText = currentEditor.document.getText(currentEditor.selection);
+    if(selectedText===""){
+        selectedText=
+        currentEditor.document.getText(
+            currentEditor.document.getWordRangeAtPosition(currentEditor.selection.active)
+        );
+    }
+    return selectedText;
+}
+
+function runInTerminal(cs : CryptolState){
+    prepareTerminal(cs.cryptolTerminal, cs.cryptolTerminalOpen);
+    let currentEditor = vscode.window.activeTextEditor;
+    if(currentEditor){
+        cs.cryptolTerminal.sendText(":l " + currentEditor.document.fileName);
+        cs.loadedFile = currentEditor.document.fileName;
+    }
+}
+
+function prepareTerminal(cryptolTerminal: vscode.Terminal, cryptolTerminalOpen : boolean) : vscode.Terminal {
+    if(!cryptolTerminalOpen){
+        let terminalPath:string = vscode.workspace.getConfiguration('cryptol').get('path','cryptol');
+        console.log(terminalPath);
+        cryptolTerminal = vscode.window.createTerminal("Cryptol extension REPL", terminalPath);
+        cryptolTerminalOpen=true;
+    }
+    cryptolTerminal.show(true);
+    return cryptolTerminal;
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -58,37 +100,6 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     context.subscriptions.push(disposable);
-}
-
-function selectionOrWord(currentEditor : vscode.TextEditor) : string {
-    let selectedText = currentEditor.document.getText(currentEditor.selection);
-    if(selectedText===""){
-        selectedText=
-        currentEditor.document.getText(
-            currentEditor.document.getWordRangeAtPosition(currentEditor.selection.active)
-        );
-    }
-    return selectedText;
-}
-
-function runInTerminal(cs : CryptolState){
-    prepareTerminal(cs.cryptolTerminal, cs.cryptolTerminalOpen);
-    let currentEditor = vscode.window.activeTextEditor;
-    if(currentEditor){
-        cs.cryptolTerminal.sendText(":l " + currentEditor.document.fileName);
-        cs.loadedFile = currentEditor.document.fileName;
-    }
-}
-
-function prepareTerminal(cryptolTerminal: vscode.Terminal, cryptolTerminalOpen : boolean) : vscode.Terminal {
-    if(!cryptolTerminalOpen){
-        let terminalPath:string = vscode.workspace.getConfiguration('cryptol').get('path','cryptol');
-        console.log(terminalPath);
-        cryptolTerminal = vscode.window.createTerminal("Cryptol extension REPL", terminalPath);
-        cryptolTerminalOpen=true;
-    }
-    cryptolTerminal.show(true);
-    return cryptolTerminal;
 }
 
 // this method is called when your extension is deactivated
